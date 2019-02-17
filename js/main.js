d1c09c56800c1c3d83b4d277c77c9a1292ba3d18
squirrel <seo.squirrel@yandex.ru>
2019-02-18 01:49:27 +0300

    небольшие правки элементов главной страницы

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   $(".owl-carousel").owlCarousel({
   	nav:true,
   	items:1,
-  	dots:false,
+  	dots:true,
   	loop:true,
   	navText:['<svg xmlns="http://www.w3.org/2000/svg" width="22" height="40" viewBox="0 0 22 40" fill="#fff"><path d="M19.83-.035l2.089 2.099L4.13 19.947l17.847 17.939-2.09 2.099L-.048 19.947z"/></svg>','<svg xmlns="http://www.w3.org/2000/svg" width="22" height="40" viewBox="0 0 22 40" fill="#fff"><path d="M2.098-.035L.009 2.064l17.789 17.883L-.048 37.886l2.089 2.099 19.935-20.038z"/></svg>'],
   });
